1751f473db9189276899649bc0259c886b433bd6
NevisMuir <rmuir310@gmail.com>
2022-05-26 16:29:07 -0400

    Solve problem 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c=(f-32)*(5/9)
+ let result=Math.round(c*10)/10
+  return result
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+let f=c*(9/5)+32
+let result=Math.round(f*10)/10
+return result
 };
 
 // Do not edit below this line
