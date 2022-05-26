1d13f6a9277429c106728a3c8ad8ea62603e8287
NevisMuir <rmuir310@gmail.com>
2022-05-26 14:12:40 -0400

    solve problem 5 used a diff solution than the odin project but i feel pretty good about this one

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(startNumber, endNumber) {
+  let sum=0
+  if(typeof startNumber !== "number" || typeof endNumber !== "number") return "ERROR"
+  if(startNumber>endNumber){
+    for(let i=endNumber;i<=startNumber;i++){
+        sum +=i
+      }
+  }else{
+    for(let i=startNumber;i<=endNumber;i++){
+      sum +=i
+    }
+  }
+    if(!(sum>=0)) return "ERROR";
+  return sum
 };
 
 // Do not edit below this line
