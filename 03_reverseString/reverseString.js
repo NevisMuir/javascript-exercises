c9b2e196db36574506c61c4c393da263fd3fafba
NevisMuir <rmuir310@gmail.com>
2022-05-25 20:08:01 -0400

    Solve 3 using array commands, rm index.html was using index.html to test my code. Found it easier to actually code and see what was happening there.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,12 +1,8 @@
 const reverseString = function(string) {
-    let length=string.length-1
-    let newString="";
-    for(let i=length;i>=0;i--){
-        let letter = string.charAt(i)
-        newString += letter
-    }
-    return newString
-};
+    return string.split('').reverse().join('');
+   };
+   
+   module.exports = reverseString;
 
 // Do not edit below this line
 module.exports = reverseString;
