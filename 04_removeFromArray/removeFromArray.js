59c1506ec110bf2c254d2878136a61ce204a580c
NevisMuir <rmuir310@gmail.com>
2022-05-26 13:06:49 -0400

    solve problem 4 included the html file i used to initially write and test my code. struggled with this one for a while!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array,...numbers) {
+      //loop starting at initial numbers position and looping until end
+  for(let i=0;i<numbers.length;i++){
+    //function to check each element in array, return when true
+    function findNumber(x){
+      return x===numbers[i]
+    }
+    //finds actual index in array when findNumber is true
+    let index=array.findIndex(findNumber)
+    //remove value from array at index
+    if(index>=0)array.splice(index,1);
+  }return array
+}
+;
 
 // Do not edit below this line
 module.exports = removeFromArray;
