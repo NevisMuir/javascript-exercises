01badd562a310379515f360461ffc66eb69b3d85
NevisMuir <rmuir310@gmail.com>
2022-06-03 14:34:35 -0400

    Complete problem 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let number = Number(index)
+    if(number<=0)return "OOPS";
+    let fib = [0,1]
+    for (let i = 2;i<=number;i++){
+        fib.push(fib[i-1]+fib[i-2])
+    }
+    return fib[number]
 };
 
 // Do not edit below this line
