d40dbc3ce127409be89b777d462e15465001c3c3
NevisMuir <rmuir310@gmail.com>
2022-06-03 15:27:32 -0400

    solve problem 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldest=array.sort(function(a,b){
+        function age (x){
+            if (isNaN(x.yearOfDeath)){return new Date().getFullYear() - x.yearOfBirth}
+            else{return x.yearOfDeath-x.yearOfBirth}
+        }
+        
+        return age(a) > age(b) ? -1: 1;
+    })
+    return oldest[0]
 };
 
 // Do not edit below this line
